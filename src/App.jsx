8f78d7a14cbaf1dac3b32300bcbfabd2a6c69e9b
Erinn Nelson <erinn@Erinns-MacBook-Pro.local>
2019-06-26 17:10:56 -0400

    setup boilerplate

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+import { Link, Route, } from 'react-router-dom'
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+
+    }
+  }
+  render() {
+
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default App;
